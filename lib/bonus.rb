a69304597fb8caff7194464d44fd5cf8c4373487
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-28 02:54:51 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,10 @@ def bonus
    }
   }
  
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
-  
-  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = 
+  "dead"
+  epic_tragedy[:montague][:hero][:status] = 
+  "dead"
  
   epic_tragedy
 end
